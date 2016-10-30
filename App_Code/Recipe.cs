8dd311bd5e2db4ffaf8a4c9f0939c98407c7b9f1
Bohdankm22 <bogdan.sharipov@yandex.ru>
2016-10-30 16:43:46 -0400

    Adding class Recipe

---
+++ App_Code/Recipe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Recipe
+/// </summary>
+public class Recipe
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public Recipe(string name, string description)
+    {
+        Name = name;
+        Description = description;
+    }
+}
