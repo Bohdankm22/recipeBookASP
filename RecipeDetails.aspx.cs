352d241e4c20d8a79b480ebe46894fdb0a80ea1a
Bohdankm22 <bogdan.sharipov@yandex.ru>
2016-12-04 21:35:10 -0500

    Added feature to edit a recipe.

--- RecipeDetails.aspx.cs
+++ RecipeDetails.aspx.cs
@@ -15,15 +15,31 @@ public partial class RecipeDetails : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        if (IsPostBack)
-        {
-            DataList1.Visible = false;
-            Label1.Visible = true;
-        }
-
         if (Request.QueryString["RecipeId"] == null)
         {
             Response.Redirect("Recipes.aspx");
         }
+
+        if (IsPostBack)
+        {
+            DataList1.DataBind();
+        }
+
+        
+    }
+
+    protected void afterDeleting(object sender, EventArgs e)
+    {
+        Response.Redirect("Recipes.aspx");
+    }
+
+    protected void Unnamed3_Click(object sender, EventArgs e)
+    {
+        DropDownList ddl = (DropDownList)DetailsView1.Rows[5].Cells[1].FindControl("DropDownList1");
+        TextBox tb = (TextBox)DetailsView1.Rows[5].Cells[1].FindControl("categoryTextBox"); 
+        String text = tb.Text;
+        tb.Text = "";
+        ddl.Items.Add(new ListItem(text, text));
+        ddl.SelectedValue = text;
     }
 }
