67c251b8647b798dfc148c34ea65b32813dd4716
Bohdankm22 <bogdan.sharipov@yandex.ru>
2016-12-11 14:33:25 -0500

    Adding authentication to the web site

--- MasterPage.master.cs
+++ MasterPage.master.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -15,5 +16,10 @@ public partial class MasterPage : System.Web.UI.MasterPage
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void singOutBtn_Click(object sender, EventArgs e)
+    {
+        FormsAuthentication.SignOut();
     }
 }
